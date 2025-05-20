8a129dab22d2ea960b8ba8858f4950d99abbae16
Dagadev <gacub20ru@gmail.com>
2025-05-20 16:37:35 -0600

    Get dev_mode in environment variables

--- me/config.py
+++ me/config.py
@@ -6,7 +6,8 @@ load_dotenv()
 api_id = getenv('API_ID')
 api_id = int(api_id) if api_id else 0
 api_hash = getenv('API_HASH') or ''
-dev_mode = False
+dev_mode = getenv('DEV_MODE') != None
+print(dev_mode)
 
 obs_host = getenv('OBS_HOST') or 'localhost'
 obs_port = getenv('OBS_PORT') or '4444'
