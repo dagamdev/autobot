4b92a3ebe184ad3dcf15071de6f85937e4f7c73d
Dagadev <gacub20ru@gmail.com>
2025-05-20 16:41:29 -0600

    Remove print

--- me/config.py
+++ me/config.py
@@ -7,7 +7,6 @@ api_id = getenv('API_ID')
 api_id = int(api_id) if api_id else 0
 api_hash = getenv('API_HASH') or ''
 dev_mode = getenv('DEV_MODE') != None
-print(dev_mode)
 
 obs_host = getenv('OBS_HOST') or 'localhost'
 obs_port = getenv('OBS_PORT') or '4444'
